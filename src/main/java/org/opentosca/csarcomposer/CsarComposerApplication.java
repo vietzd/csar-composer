89d2c47a20aa020fd89b3446d44b0fdb61fd1697
Daniel Vietz <vietzd@gmail.com>
2018-06-19 15:38:50 +0200

    Initialized Spring Boot Project

---
+++ src/main/java/org/opentosca/csarcomposer/CsarComposerApplication.java
@@ -0,0 +1,12 @@
+package org.opentosca.csarcomposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsarComposerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsarComposerApplication.class, args);
+    }
+}
---
+++ src/test/java/org/opentosca/csarcomposer/CsarComposerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.opentosca.csarcomposer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsarComposerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
