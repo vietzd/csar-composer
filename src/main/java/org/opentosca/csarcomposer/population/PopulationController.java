eebd990f5d925708cd024ef60e437c89075076c0
Daniel Vietz <vietzd@gmail.com>
2018-06-19 17:32:09 +0200

    Add REST methods to get CSARs from source- and internal repository

--- src/main/java/org/opentosca/csarcomposer/population/PopulationController.java
+++ src/main/java/org/opentosca/csarcomposer/population/PopulationController.java
@@ -2,10 +2,15 @@ package org.opentosca.csarcomposer.population;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class PopulationController {
 
     @Autowired
@@ -17,5 +22,15 @@ public class PopulationController {
         mv.addObject("sourceCsars", populationService.getAllSourceCsars());
         mv.addObject("internalCsars", populationService.getAllInternalCsars());
         return mv;
+    }
+
+    @GetMapping("/sourceCsars")
+    public List<CSAR> getSourceCsars(Model m) {
+        return populationService.getAllSourceCsars();
+    }
+
+    @GetMapping("/internalCsars")
+    public List<CSAR> getInternalCsars(Model m) {
+        return populationService.getAllInternalCsars();
     }
 }
