2c4ff45cc8fca64217e577d5fd57648acb81713b
Daniel Vietz <vietzd@gmail.com>
2018-06-19 17:53:21 +0200

    Add project lombok to remove boilerplate from data classes

--- src/main/java/org/opentosca/csarcomposer/population/CSAR.java
+++ src/main/java/org/opentosca/csarcomposer/population/CSAR.java
@@ -1,29 +1,14 @@
 package org.opentosca.csarcomposer.population;
 
+import lombok.Data;
 import org.eclipse.winery.common.ids.definitions.ServiceTemplateId;
 
+@Data
 public class CSAR {
     private ServiceTemplateId serviceTemplateId;
     private String name;
 
     public CSAR(ServiceTemplateId serviceTemplateId) {
         this.serviceTemplateId = serviceTemplateId;
-    }
-
-    public ServiceTemplateId getServiceTemplateId() {
-        return serviceTemplateId;
-    }
-
-    public void setServiceTemplateId(ServiceTemplateId serviceTemplateId) {
-        this.serviceTemplateId = serviceTemplateId;
-    }
-
-    public String getName() {
-        return name;
-
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
