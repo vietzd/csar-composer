8ea86b9a25f52bc2ef07b3e285d49a7e0d3476ab
Daniel <vietzd@gmail.com>
2018-07-16 11:56:33 +0200

    Implement filter service

--- src/main/java/org/opentosca/csarcomposer/filter/FilterService.java
+++ src/main/java/org/opentosca/csarcomposer/filter/FilterService.java
@@ -1,8 +1,11 @@
 package org.opentosca.csarcomposer.filter;
 
+import org.eclipse.winery.common.ids.definitions.CapabilityTypeId;
 import org.opentosca.csarcomposer.model.Csar;
+import org.opentosca.csarcomposer.model.Requirement;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -10,6 +13,36 @@ import java.util.function.Predicate;
 public class FilterService {
 
     public Predicate<? super Csar> createFilter(List<Csar> internalRepository) {
-        return csar -> !internalRepository.contains(csar);
+        List<Requirement> usedRequirements = new ArrayList<>();
+        List<CapabilityTypeId> usedCapabilities = new ArrayList<>();
+        internalRepository.forEach(internalCsar -> usedRequirements.addAll(internalCsar.getRequirements()));
+        internalRepository.forEach(internalCsar -> usedCapabilities.addAll(internalCsar.getCapabilities()));
+
+        return (Predicate<Csar>) csar -> {
+
+            // Don't allow any capability twice
+            for (CapabilityTypeId usedCapability : usedCapabilities) {
+                if (csar.getCapabilities().contains(usedCapability)) {
+                    return false;
+                }
+            }
+
+            // Allow all capabilities that fulfill any active requirement
+            for (Requirement usedRequirement : usedRequirements) {
+                CapabilityTypeId requiredCapability = usedRequirement.getRequiredCapabilityType();
+                if (csar.getCapabilities().contains(requiredCapability)) {
+                    return true;
+                }
+            }
+
+            // Allow all requirements that use any active capability
+            for (Requirement requirement : csar.getRequirements()) {
+                if (usedCapabilities.contains(requirement.getRequiredCapabilityType())) {
+                    return true;
+                }
+            }
+
+            return true; // TODO return false
+        };
     }
 }
